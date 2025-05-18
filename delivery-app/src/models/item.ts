a42d8e95cae342e1f78aa0c6601fa116ee2e6d89
dolevoper <omerdolev90@gmail.com>
2025-05-18 20:17:27 +0300

    cors and get items from server

--- delivery-app/src/models/item.ts
+++ delivery-app/src/models/item.ts
@@ -1,10 +1,10 @@
-import burgerImg from "../assets/burger.jpg";
-import frenchFriesImg from "../assets/french fries.jpg";
-import sodaImg from "../assets/soda.jpg";
-import mayoImg from "../assets/mayo.jpg";
+// import burgerImg from "../assets/burger.jpg";
+// import frenchFriesImg from "../assets/french fries.jpg";
+// import sodaImg from "../assets/soda.jpg";
+// import mayoImg from "../assets/mayo.jpg";
 
 export type Item = {
-    id: string,
+    _id: string,
     name: string,
     description: string,
     imgSrc: string,
@@ -12,24 +12,8 @@ export type Item = {
 };
 
 export async function getItems(): Promise<Item[]> {
-    await randomDelay();
-    
-    return [
-        { id: "1111", name: "Burger", description: "A yummy burger", imgSrc: burgerImg, priceInAgorot: 4000 },
-        { id: "2222", name: "French fries", description: "Crispy, delicious, french frize", imgSrc: frenchFriesImg, priceInAgorot: 2500 },
-        { id: "3333", name: "Soda", description: "Fizzling soda", imgSrc: sodaImg, priceInAgorot: 1000 },
-        { id: "4444", name: "Mayo", description: "Our home-made mayo sauce", imgSrc: mayoImg, priceInAgorot: 200 },
-    ];
+    const res = await fetch("http://localhost:5000/items");
+    const items = await res.json();
+
+    return items;
 }
-
-const randomDelay = () => new Promise<void>((resolve) => {
-    const delay = (Math.random() * 2000) + 700;
-
-    return setTimeout(
-        () => {
-            resolve();
-        },
-        delay
-    );
-});
-
--- delivery-service/src/app.ts
+++ delivery-service/src/app.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import { json } from "body-parser";
+import cors from "cors";
 import { router } from "./routers/items";
 
 export const app = express();
+
+app.use(cors());
 
 app.use((req, _, next) => {
     console.log(new Date(), req.method, req.url);
     next();
 });
-
+ 
 app.use(json());
 
 app.use("/items", router);
